d8215a2390257045307f4b9a6c9db105dd2d8ab9
forger6969 <saidazim186@gmail.com>
2025-12-03 04:04:03 +0500

    save

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Footer from './components/Footer'
 import SwiperForger from './components/SwiperForger'
 
 function App() {
+  
   return (
     <main className="relative min-h-screen overflow-x-hidden bg-[#E9ECEF]">
       <div className="absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10"></div>
@@ -27,6 +28,7 @@ function App() {
         <TestimonialsSection />
         <NewsletterSection />
         <Footer />
+
       </div>
     </main>
   )
